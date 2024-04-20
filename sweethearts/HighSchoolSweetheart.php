6a22196a89cc4230c48b6020c90b4342cadac654
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-04-20 11:08:16 -0400

    problem solved

--- sweethearts/HighSchoolSweetheart.php
+++ sweethearts/HighSchoolSweetheart.php
@@ -4,21 +4,46 @@ class HighSchoolSweetheart
 {
     public function firstLetter(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return substr(trim($name), 0, 1);
     }
 
     public function initial(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return $this->firstLetter(strtoupper($name)) . ".";
     }
 
     public function initials(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        $words = explode(" ", $name);
+        $initials = [];
+
+        foreach ($words as $word) {
+            $initials[] = $this->initial($word);
+        }
+
+        return implode(" ", $initials);
     }
 
     public function pair(string $sweetheart_a, string $sweetheart_b): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        $initials_a = $this->initials($sweetheart_a);
+        $initials_b = $this->initials($sweetheart_b);
+
+        return <<<EXPECTED_HEART
+                 ******       ******
+               **      **   **      **
+             **         ** **         **
+            **            *            **
+            **                         **
+            **     $initials_a  +  $initials_b     **
+             **                       **
+               **                   **
+                 **               **
+                   **           **
+                     **       **
+                       **   **
+                         ***
+                          *
+            EXPECTED_HEART;
     }
 }
